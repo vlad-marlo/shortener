8b4e1f7bf4319464bc611439c762b4864c034eb8
marlo <narl0durn0@ya.ru>
2022-08-02 00:35:41 +0300

    fixed punctuation in errors

--- internal/store/errors.go
+++ internal/store/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	ErrNotFound      error = errors.New("not found")
-	ErrAlreadyExists error = errors.New("already in storage.")
+	ErrAlreadyExists error = errors.New("already in storage")
 )
