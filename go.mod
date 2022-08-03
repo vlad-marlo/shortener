f45271882c4d2a77e447639a06655c40b0217997
marlo <narl0durn0@ya.ru>
2022-08-03 19:47:33 +0300

    created all store tests for inmemory repository

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/vlad-marlo/shortener
 
 go 1.18
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- internal/store/inmemory/store_test.go
+++ internal/store/inmemory/store_test.go
@@ -1,10 +1,22 @@
 package inmemory
 
 import (
-	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/vlad-marlo/shortener/internal/store"
 	"github.com/vlad-marlo/shortener/internal/store/model"
+)
+
+var (
+	googleURL = model.URL{
+		ID:      "goo",
+		BaseURL: "https://google.com/",
+	}
+	yandexURL = model.URL{
+		ID:      "ya",
+		BaseURL: "yandex.ru",
+	}
 )
 
 func TestStore_Create(t *testing.T) {
@@ -19,14 +31,22 @@ func TestStore_Create(t *testing.T) {
 	}{
 		{
 			name: "with empty urls",
-			u: model.URL{
-				ID:      "some-text",
-				BaseURL: "https://google.com/",
-			},
+			u:    googleURL,
 			fields: fields{
 				urls: []model.URL{},
 			},
 			wantErr: false,
+		},
+		{
+			name: "with duplicates",
+			u:    googleURL,
+			fields: fields{
+				urls: []model.URL{
+					googleURL,
+					yandexURL,
+				},
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -34,8 +54,15 @@ func TestStore_Create(t *testing.T) {
 			s := &Store{
 				urls: tt.fields.urls,
 			}
-			if err := s.Create(tt.u); (err != nil) != tt.wantErr {
-				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+			err := s.Create(tt.u)
+			if tt.wantErr {
+				assert.Error(t, err, "Create() error is nil")
+				if err != store.ErrAlreadyExists {
+					assert.NotContains(t, s.urls, tt.u)
+				}
+			} else {
+				assert.NoError(t, err, "Create() error = %v", err)
+				assert.Contains(t, s.urls, tt.u)
 			}
 		})
 	}
@@ -63,13 +90,13 @@ func TestStore_GetByID(t *testing.T) {
 				urls: tt.fields.urls,
 			}
 			got, err := s.GetByID(tt.args.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetByID() error = %v, wantErr %v", err, tt.wantErr)
+			if tt.wantErr {
+				assert.Error(t, err, "GetByID() error = %v, wantErr %v", err, tt.wantErr)
 				return
+			} else {
+				assert.NoError(t, err, "There must be error got %v", err)
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetByID() got = %v, want %v", got, tt.want)
-			}
+			assert.True(t, assert.ObjectsAreEqual(got, tt.want), "GetByID() got = %v, want %v", got, tt.want)
 		})
 	}
 }
@@ -86,17 +113,14 @@ func TestStore_urlExists(t *testing.T) {
 		fields fields
 		args   args
 		want   bool
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &Store{
 				urls: tt.fields.urls,
 			}
-			if got := s.urlExists(tt.args.url); got != tt.want {
-				t.Errorf("urlExists() = %v, want %v", got, tt.want)
-			}
+			got := s.urlExists(tt.args.url)
+			assert.Equal(t, got, tt.want, "urlExists() = %v, want %v", got, tt.want)
 		})
 	}
 }
