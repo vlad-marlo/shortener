f34d12dc0a78c30fdbd19c2e6a623823d8594bed
marlo <narl0durn0@ya.ru>
2022-08-03 22:02:22 +0300

    created tests for / endpoint

--- internal/httpserver/handlers_test.go
+++ internal/httpserver/handlers_test.go
@@ -1,37 +1,61 @@
 package httpserver
 
 import (
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/vlad-marlo/shortener/internal/store"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestServer_handleURLGetCreate(t *testing.T) {
-	type fields struct {
-		Server http.Server
-		Store  store.Store
-		Config *Config
-	}
+	s := New(NewConfig())
 	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+		method          string
+		url             string
+		body            io.Reader
+		bodyResponse    bool
+		headersResponse bool
+	}
+	type want struct {
+		statusCode int
+		headers    http.Header
+		body       string
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name string
+		args args
+		want want
 	}{
 		// TODO: add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Server{
-				srv:    tt.fields.Server,
-				Store:  tt.fields.Store,
-				Config: tt.fields.Config,
+			request := httptest.NewRequest(tt.args.method, tt.args.url, tt.args.body)
+
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(s.handleURLGetCreate)
+			h.ServeHTTP(w, request)
+
+			res := w.Result()
+
+			assert.Equal(t, res.StatusCode, tt.want.statusCode)
+
+			if tt.args.headersResponse {
+				for k, v := range tt.want.headers {
+					assert.Equal(t, res.Header.Get(k), v, "Header[%v] want %v got %v", k, v, res.Header.Get(k))
+				}
 			}
-			s.handleURLGetCreate(tt.args.w, tt.args.r)
+
+			if tt.args.bodyResponse {
+				defer res.Body.Close()
+				resBody, err := io.ReadAll(res.Body)
+				assert.NoError(t, err)
+
+				// checking response answer
+				assert.Equal(t, string(resBody), tt.want.body)
+			}
 		})
 	}
 }
