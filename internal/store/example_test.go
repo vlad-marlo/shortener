45108eeb92d8ecdb6e5e7beadb6f7322872909b8
vlad <vlad.marlo@ya.ru>
2022-11-21 23:04:16 +0300

    ready for review

--- internal/store/example_test.go
+++ internal/store/example_test.go
@@ -1,15 +1,13 @@
 package store_test
 
 import (
-	"github.com/vlad-marlo/shortener/internal/store"
 	"github.com/vlad-marlo/shortener/internal/store/inmemory"
 )
 
 func Example() {
-	var storage store.Store
 	// init storage
 	// for example inmemory storage
-	storage = inmemory.New()
+	storage := inmemory.New()
 	defer func() {
 		// always close storage after work with it
 		if err := storage.Close(); err != nil {
